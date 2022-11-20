5aaef9783e532a1b0b8e3f16243d38ce8e96416e
pilgyu14 <pilgue0822@naver.com>
2022-11-20 23:48:33 +0900

    1120_agent 베이스

--- Assets/01.Scripts/Agent/Player/PlayerController.cs
+++ Assets/01.Scripts/Agent/Player/PlayerController.cs
@@ -22,16 +22,23 @@ public class TestE
 
 
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IAgent ,IDamagable
 {
+    // 인스펙터 
+    private AgentSO _playerSO;
+
     // 캐싱 변수 
     private InputModule _inputModule;
     private CharacterController _chController;
     private PlayerAnimation _playerAnimation; 
 
+    // 내부 변수 
+    private bool _isDie = false; 
 
-    private float _speed = 10;
-
+    // 프로퍼티
+    public bool IsDie => _isDie; 
+    public float HP => _playerSO.hp; 
+    
     private void Awake()
     {
         _inputModule = GetComponent<InputModule>();
@@ -46,14 +53,19 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        _chController.Move(_inputModule.MoveDir * _speed * Time.deltaTime);
+        _chController.Move(_inputModule.MoveDir * _playerSO.speed * Time.deltaTime);
         if(_chController.velocity .magnitude> 0.2f)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_inputModule.MoveDir, Vector3.up),
-            Time.deltaTime * _speed);
+            Time.deltaTime * _playerSO.speed);
         }
-
-  
     }
 
+    public void Damaged()
+    {
+    }
+
+    public void OnDie()
+    {
+    }
 }
