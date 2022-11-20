f962eb03d51fe6356cec18e4f428c2aeabd73bb1
pilgyu14 <pilgue0822@naver.com>
2022-11-20 23:30:56 +0900

    1120_플레이어 이동, 애니메이터 제작

--- Assets/01.Scripts/Agent/Player/PlayerAnimation.cs
+++ Assets/01.Scripts/Agent/Player/PlayerAnimation.cs
@@ -2,17 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour
+public class PlayerAnimation : AgentAnimation
 {
-    // Start is called before the first frame update
-    void Start()
+    // Hash
+
+    // 이동 
+    private readonly int _velocityXHash = Animator.StringToHash("VelocityX");
+    private readonly int _velocityYHash = Animator.StringToHash("VelocityY");
+
+    // 공격 
+    private readonly int _frontKickHash = Animator.StringToHash("FrontKick");
+    private readonly int _sideKickHash = Animator.StringToHash("SideKick");
+    private readonly int _backKickHash = Animator.StringToHash("BackKick");
+    private readonly int _tackleHash = Animator.StringToHash("Tackle");
+
+    protected override void Awake()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // 킥 애니메이션 
+    public void SetFrontKick()
     {
-        
+        _agentAnimator.SetTrigger(_frontKickHash);
     }
+    public void SetSideKick()
+    {
+        _agentAnimator.SetTrigger(_sideKickHash); 
+    }
+    public void SetBackKick()
+    {
+        _agentAnimator.SetTrigger(_backKickHash);
+    }
+
+    public void SetTackle()
+    {
+        _agentAnimator.SetTrigger(_tackleHash);
+    }
+
+    // 이동 설정 
+    public void SetVelocity(float x, float y)
+    {
+        _agentAnimator.SetFloat(_velocityXHash, x);
+        _agentAnimator.SetFloat(_velocityYHash, y);
+    }
+
 }
--- Assets/01.Scripts/Agent/Player/PlayerController.cs
+++ Assets/01.Scripts/Agent/Player/PlayerController.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.AI;
+
+public class TestE
+{
+    private NavMeshAgent _agent; 
+   
+    void A()
+    {
+    }
+    
+}
+
+
+// 상하 좌우 이동 
+// 회전 
+// 액션 
+// 애니메이션 
+
+
+
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 캐싱 변수 
+    private InputModule _inputModule;
+    private CharacterController _chController;
+    private PlayerAnimation _playerAnimation; 
+
+
+    private float _speed = 10;
+
+    private void Awake()
     {
-        
+        _inputModule = GetComponent<InputModule>();
+        _chController = GetComponent<CharacterController>();
+        _playerAnimation = GetComponent<PlayerAnimation>(); 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move(); 
     }
+
+    private void Move()
+    {
+        _chController.Move(_inputModule.MoveDir * _speed * Time.deltaTime);
+        if(_chController.velocity .magnitude> 0.2f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_inputModule.MoveDir, Vector3.up),
+            Time.deltaTime * _speed);
+        }
+
+  
+    }
+
 }
